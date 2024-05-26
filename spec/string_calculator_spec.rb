64c9ab1370ebcbce8df44f7e939aa7bf7d790c12
Priya Raghavan <priyabhuvanaj99@gmail.com>
2024-05-26 13:22:46 +0530

    support delimiter string addition

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -4,7 +4,7 @@ require 'rspec'
 require_relative '../string_calculator'
 
 RSpec.describe StringCalculator do
-  let(:calculator) { StringCalculator.new }
+  let(:calculator) { described_class.new }
 
   it "returns 0 for an empty string" do
     expect(calculator.add("")).to eq(0)
@@ -37,6 +37,28 @@ RSpec.describe StringCalculator do
     it "returns the sum of numbers with new lines" do
       expect(calculator.add("1\n2,3")).to eq(6)
       expect(calculator.add("4\n5,6")).to eq(15)
+    end
+  end
+
+  context "supporting different delimiters" do
+    it "supports different single-character delimiters" do
+      expect(calculator.add("//;\n1;2")).to eq(3)
+      expect(calculator.add("//|\n1|2|3")).to eq(6)
+    end
+
+    it "supports delimiters of any length" do
+      expect(calculator.add("//[***]\n1***2***3")).to eq(6)
+      expect(calculator.add("//[###]\n4###5###6")).to eq(15)
+    end
+
+    it "supports multiple delimiters" do
+      expect(calculator.add("//[*][%]\n1*2%3")).to eq(6)
+      expect(calculator.add("//[;][&]\n4;5&6")).to eq(15)
+    end
+
+    it "supports multiple delimiters with length longer than one char" do
+      expect(calculator.add("//[***][%%%]\n1***2%%%3")).to eq(6)
+      expect(calculator.add("//[###][&&&]\n4###5&&&6")).to eq(15)
     end
   end
 end
--- string_calculator.rb
+++ string_calculator.rb
@@ -1,6 +1,27 @@
 class StringCalculator
   def add(numbers)
     return 0 if numbers.empty?
-    numbers.split(/,|\n/).map(&:to_i).sum
+
+    delimiter = /,|\n/
+    if numbers.start_with?("//")
+      parts = numbers.split("\n", 2)
+      delimiters = extract_delimiters(parts[0])
+      delimiter = Regexp.union(delimiters)
+      numbers = parts[1]
+    end
+
+    num_array = numbers.split(delimiter).map(&:to_i)
+    
+    num_array.sum
+  end
+
+  private
+
+  def extract_delimiters(header)
+    if header.start_with?("//[") && header.end_with?("]")
+      header[3..-2].split('][')
+    else
+      [header[2]]
+    end
   end
 end
